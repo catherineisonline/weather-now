85e63fe601db3fc6e1829c3a7cf86bb1ec02ceb5
Ryan <rayantovi5@gmail.com>
2023-05-01 00:53:31 +0300

    Implemented user IP location weather upon land

--- src/App.js
+++ src/App.js
@@ -1,120 +1,190 @@
-
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Main from "./components/main/Main";
 import { useEffect, useState } from "react";
 import GlobalStyles from "./core-ui/Globals";
 import { ThemeProvider } from "styled-components";
-import { defaultWeather, clouds, rain, clear, thunderstorm, snow, drizzle, mist, smoke, fog, haze } from "./core-ui/Themes.styled";
-
+import {
+  defaultWeather,
+  clouds,
+  rain,
+  clear,
+  thunderstorm,
+  snow,
+  drizzle,
+  mist,
+  smoke,
+  fog,
+  haze,
+} from "./core-ui/Themes.styled";
 
 function App() {
-  const [todayWeather, setTodayWeather] = useState({ name: "", country: "", temp: "", icon: "03d", weather: "", weatherDesc: "", feelsLike: "", humidity: "", wind: "", highest: "", lowest: "" });
   const [searchedLocation, setSearchedLocation] = useState("Buenos Aires");
   const [lat, setLat] = useState("");
   const [lon, setLon] = useState("");
   const [searchDone, setSearchDone] = useState(false);
-  const [theme, setTheme] = useState('clear');
+  const [theme, setTheme] = useState("clear");
   const [formValue, setFormValue] = useState({ searchedLocation: "" });
   const [formError, setFormError] = useState({});
   const [noData, setNoData] = useState(false);
   const [loading, setLoading] = useState("");
 
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `http://api.ipapi.com/api/check?access_key=${process.env.REACT_APP_VERY_PRIVATE_KEY2}` //New API
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res, json);
+        let user_ip_location = res.city;
+        setSearchedLocation((prev) => user_ip_location);
+      });
+  }, []);
+  const [todayWeather, setTodayWeather] = useState({
+    name: "",
+    country: "",
+    temp: "",
+    icon: "03d",
+    weather: "",
+    weatherDesc: "",
+    feelsLike: "",
+    humidity: "",
+    wind: "",
+    highest: "",
+    lowest: "",
+  });
   const handleSubmit = (e) => {
-
     e.preventDefault();
     setFormError(validateForm(formValue));
     // setSubmit(true);
     setSearchedLocation(formValue.searchedLocation);
     setFormValue({ searchedLocation: "" });
-  }
+  };
   const handleValidation = async (e) => {
     const { name, value } = e.target;
     setFormValue({ ...formValue, [name]: value });
-  }
+  };
   const validateForm = (value) => {
     let errors = {};
     if (!value.searchedLocation) {
-      errors.searchedLocation = "Please enter a country name"
+      errors.searchedLocation = "Please enter a country name";
     }
-    
-    
+
     return errors;
-  }
+  };
 
-  const setWeather = theme === "rain" ? rain : theme === "clouds" ? clouds : theme === "clear" ? clear : theme === "thunderstorm" ? thunderstorm : theme === "snow" ? snow : theme === "drizzle" ? drizzle : theme === "mist" ? mist : theme === "smoke" ? smoke : theme === "haze" ? haze : theme === "fog" ? fog : defaultWeather;
-
+  const setWeather =
+    theme === "rain"
+      ? rain
+      : theme === "clouds"
+      ? clouds
+      : theme === "clear"
+      ? clear
+      : theme === "thunderstorm"
+      ? thunderstorm
+      : theme === "snow"
+      ? snow
+      : theme === "drizzle"
+      ? drizzle
+      : theme === "mist"
+      ? mist
+      : theme === "smoke"
+      ? smoke
+      : theme === "haze"
+      ? haze
+      : theme === "fog"
+      ? fog
+      : defaultWeather;
 
   useEffect(() => {
     setLoading(true);
     setNoData(false);
-    
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedLocation}&limit=1&appid=${process.env.REACT_APP_VERY_PRIVATE_KEY}`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${searchedLocation}&limit=1&appid=${process.env.REACT_APP_VERY_PRIVATE_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         setLat(data[0]?.lat);
         setLon(data[0]?.lon);
         setSearchDone(true);
-        setTodayWeather(prev => { return { ...prev, name: data[0]?.local_names?.en, country: data[0]?.country } })
-       
-      }).catch((err) => {
+        setTodayWeather((prev) => {
+          return {
+            ...prev,
+            name: data[0]?.name,
+            country: data[0]?.country,
+          };
+        });
+      })
+      .catch((err) => {
         console.log(err.message);
       });
-
   }, [searchedLocation]);
 
   useEffect(() => {
     if (searchDone) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_VERY_PRIVATE_KEY}&units=metric`)
-        .then(response => response.json())
-        .then(data => {
-          setTodayWeather({ ...todayWeather,  temp: Math.ceil(data?.main?.temp), icon: data.weather[0].icon, weather: data.weather[0].main.toLowerCase(), weatherDesc: data.weather[0].description, feelsLike: data.main.feels_like, humidity: data.main.humidity, wind: data.wind.speed, highest: data.main.temp_max, lowest: data.main.temp_min });
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_VERY_PRIVATE_KEY}&units=metric`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setTodayWeather({
+           ...todayWeather,
+            temp: Math.ceil(data?.main?.temp),
+            icon: data.weather[0].icon,
+            weather: data.weather[0].main.toLowerCase(),
+            weatherDesc: data.weather[0].description,
+            feelsLike: data.main.feels_like,
+            humidity: data.main.humidity,
+            wind: data.wind.speed,
+            highest: data.main.temp_max,
+            lowest: data.main.temp_min,
+          });
           setLoading(false);
-        }).catch((err) => {
+        })
+        .catch((err) => {
           setLoading(false);
           setNoData(true);
           console.log(err.message, "errrr");
         });
     }
     const toggleTheme = () => {
-      if (todayWeather.weather === '') {
-        setTheme('default');
+      if (todayWeather.weather === "") {
+        setTheme("default");
       }
-      if (todayWeather.weather === 'clouds') {
-        setTheme('clouds');
+      if (todayWeather.weather === "clouds") {
+        setTheme("clouds");
       }
-      if (todayWeather.weather === 'rain') {
-        setTheme('rain');
+      if (todayWeather.weather === "rain") {
+        setTheme("rain");
       }
-      if (todayWeather.weather === 'clear') {
-        setTheme('clear');
+      if (todayWeather.weather === "clear") {
+        setTheme("clear");
       }
-      if (todayWeather.weather === 'thunderstorm') {
-        setTheme('thunderstorm');
+      if (todayWeather.weather === "thunderstorm") {
+        setTheme("thunderstorm");
       }
-      if (todayWeather.weather === 'snow') {
-        setTheme('snow');
+      if (todayWeather.weather === "snow") {
+        setTheme("snow");
       }
-      if (todayWeather.weather === 'drizzle') {
-        setTheme('drizzle');
+      if (todayWeather.weather === "drizzle") {
+        setTheme("drizzle");
+      } else if (todayWeather.weather === "mist") {
+        setTheme("mist");
       }
-      else if (todayWeather.weather === 'mist') {
-        setTheme('mist');
+      if (todayWeather.weather === "smoke") {
+        setTheme("smoke");
       }
-      if (todayWeather.weather === 'smoke') {
-        setTheme('smoke');
+      if (todayWeather.weather === "fog") {
+        setTheme("fog");
       }
-      if (todayWeather.weather === 'fog') {
-        setTheme('fog');
-      }
-      if (todayWeather.weather === 'haze') {
-        setTheme('haze');
+      if (todayWeather.weather === "haze") {
+        setTheme("haze");
       }
       //, Dust, Sand, Ash, Squall, Tornado
-    }
+    };
 
     toggleTheme();
     return () => setSearchDone(false);
-
   }, [searchDone, lat, lon, todayWeather, searchedLocation]);
 
   return (
@@ -122,7 +192,20 @@ function App() {
       <BrowserRouter>
         <GlobalStyles />
         <Routes>
-          <Route path="/" element={<Main noData={noData} loading={loading} formError={formError} formValue={formValue} todayWeather={todayWeather} handleSubmit={handleSubmit} handleValidation={handleValidation} />} />
+          <Route
+            path="/"
+            element={
+              <Main
+                noData={noData}
+                loading={loading}
+                formError={formError}
+                formValue={formValue}
+                todayWeather={todayWeather}
+                handleSubmit={handleSubmit}
+                handleValidation={handleValidation}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
