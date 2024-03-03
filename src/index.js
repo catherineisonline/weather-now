14d6b85f935b4cc6144a96990b5cf6adc4527224
Ekaterine Mitagvaria <ekaterine@Ekaterines-MacBook-Air.local>
2024-03-03 22:05:30 +0400

    Test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './core-ui/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
